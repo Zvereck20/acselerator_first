1dec7ee2cb373391cacdcb1ec891403350b2f861
Zvereck20 <kerusov20@gmail.com>
2021-07-08 01:41:57 +1000

    Fixed bugs

--- source/js/script.js
+++ source/js/script.js
@@ -60,6 +60,13 @@ window.addEventListener("DOMContentLoaded", function () {
       i = 0,
       def = matrix.replace(/\D/g, ""),
       val = this.value.replace(/\D/g, "");
+
+    if (val.length < 10) {
+      this.setCustomValidity('Номер введен не полностью');
+    } else {
+      this.setCustomValidity('');
+    }
+
     if (def.length >= val.length) val = def;
     this.value = matrix.replace(/./g, function (a) {
       return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
